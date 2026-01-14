579adfea6a97f9d8f6dbcbf892b23a118d68b85b
Shubham Bansal <sm325642@gmail.com>
2026-01-14 19:22:41 +0530

    final

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -15,7 +15,7 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data });
     } catch (error) {
-      console.log(error.message);
+      
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
