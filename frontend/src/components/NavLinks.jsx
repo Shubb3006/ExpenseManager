743438b51a4c01436021524f7c4a8990deb734a4
Shubb3006 <sm325642@gmail.com>
2026-01-24 15:04:29 +0530

    final

--- frontend/src/components/NavLinks.jsx
+++ frontend/src/components/NavLinks.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavLinks = ({ onClick }) => {
+  const location =useLocation();
   return (
     <div className="flex">
      
-        <Link onClick={onClick} to="/" className="btn btn-ghost w-32">
+        <Link onClick={onClick} to="/" className={`btn btn-ghost w-32 ${location.pathname==="/" ? "text-primary":"text-base-content"}`}>
          Home
         </Link>
-        <Link onClick={onClick} to="/expenses" className="btn btn-ghost w-32">
+        <Link onClick={onClick} to="/expenses" className={`btn btn-ghost w-32 ${location.pathname.startsWith("/expenses") ?"text-primary":"text-base-content"}`}>
           Expenses
         </Link>
-        <Link onClick={onClick} to="/addexpense" className="btn btn-ghost w-32">
+        <Link onClick={onClick} to="/addexpense" className={`btn btn-ghost w-32 ${location.pathname.startsWith("/addexpense") ? "text-primary":"text-base-content"}`}>
           Add Expense
         </Link>
     </div>
