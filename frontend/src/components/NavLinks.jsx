53669149af94240f8d60e0600ae42d75ed949a4e
Shubb3006 <sm325642@gmail.com>
2026-01-24 15:54:21 +0530

    final

--- frontend/src/components/NavLinks.jsx
+++ frontend/src/components/NavLinks.jsx
@@ -8,7 +8,7 @@ const NavLinks = ({ onClick }) => {
       <Link
         onClick={onClick}
         to="/"
-        className={`btn btn-ghost w-25 text-lg ${
+        className={`btn btn-ghost w-25 text-md ${
           location.pathname === "/" ? "text-primary" : "text-base-content"
         }`}
       >
@@ -17,7 +17,7 @@ const NavLinks = ({ onClick }) => {
       <Link
         onClick={onClick}
         to="/expenses"
-        className={`btn btn-ghost w-28 text-lg ${
+        className={`btn btn-ghost w-28 text-md ${
           location.pathname.startsWith("/expenses")
             ? "text-primary"
             : "text-base-content"
@@ -28,7 +28,7 @@ const NavLinks = ({ onClick }) => {
       <Link
         onClick={onClick}
         to="/addexpense"
-        className={`btn btn-ghost w-32 text-lg p-0 ${
+        className={`btn btn-ghost w-32 text-md p-0 ${
           location.pathname.startsWith("/addexpense")
             ? "text-primary"
             : "text-base-content"
